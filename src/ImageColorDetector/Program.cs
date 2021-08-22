5efffbb91be67100ec46fd2238ec5a47cb003f49
Joel Goransson <joel.goransson@bigtincan.com>
2021-08-22 18:27:01 +1000

    Initial commit, quick and dirty, multithreaded, no cli args
    Quick and dirty fix for my 67,000 images that needed filtering. Detects all images that are almost completely black or almost completely white, and moves them into a folder called "Detected".
    Multi threaded for speed, did about 67,000 in under 15ish seconds.
    Nasty basic code.
    Doesn't take any cli args unfortunately.
    Could easily be changed to do something else in future.

---
+++ src/ImageColorDetector/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageColorDetector
+{
+    class Program
+    {
+        static readonly float blackThreshold = Color.FromArgb(3, 3, 3).GetBrightness();
+        static readonly float whiteThreshold = Color.FromArgb(252, 252, 252).GetBrightness();
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter directory: ");
+            var dir = Console.ReadLine();
+
+            if (!dir.EndsWith(Path.DirectorySeparatorChar))
+            {
+                dir += Path.DirectorySeparatorChar;
+            }
+
+            List<string> detectedFiles = new();
+            object detectedFilesLock = new object();
+
+            var files = Directory.GetFiles(dir);
+
+            files = files.Select(t => t.Replace(dir, string.Empty)).ToArray();
+
+            Parallel.ForEach(files, (file) => 
+            {
+                var image = new Bitmap(Path.Combine(dir, file));
+
+                var thumb = new Bitmap(image, new Size(8, 8));
+                image.Dispose();
+
+                var avgColor = GetImageAverageColor(thumb);
+
+                var brightness = avgColor.GetBrightness();
+
+                if (brightness < blackThreshold || brightness > whiteThreshold)
+                {
+                    lock (detectedFilesLock)
+                    {
+                        detectedFiles.Add(file);
+                    }
+                }
+            });
+
+            // Do something with the detected files.
+            Directory.CreateDirectory(dir + Path.DirectorySeparatorChar + "Detected");
+
+            Parallel.ForEach(detectedFiles, (file) =>
+            {
+                File.Move(Path.Combine(dir, file), Path.Combine(dir, "Detected", file), true);
+            });
+        }
+
+        private static Color GetImageAverageColor(Bitmap img)
+        {
+            int r = 0, g = 0, b = 0;
+
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    var pixel = img.GetPixel(x, y);
+                    r += pixel.R;
+                    g += pixel.G;
+                    b += pixel.B;
+                }
+            }
+
+            var total = 8 * 8;
+
+            var rAvg = r / total;
+            var gAvg = g / total;
+            var bAvg = b / total;
+
+            return Color.FromArgb(rAvg, gAvg, bAvg);
+        }
+    }
+}
